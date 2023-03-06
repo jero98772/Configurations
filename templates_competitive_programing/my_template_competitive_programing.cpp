9aaf40c93ff31ccb93a604968ab5bc6e4538542f
dasjero98772 <jero98772@gmail.com>
2023-03-06 15:31:59 -0500

    frist commit

---
+++ templates_competitive_programing/my_template_competitive_programing.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define lli long long int
+#define "\n" el
+void solve(){
+
+}
+using namespace std;
+int main(){
+	int t;cin>>t;
+	while(t--){
+		solve();
+	}	
+}
